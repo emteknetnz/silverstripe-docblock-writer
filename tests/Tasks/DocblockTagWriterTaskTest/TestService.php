d4d15f22b141b6d7d632bb0cf48a512ff389becd
Steve Boyd <emteknetnz@gmail.com>
2023-12-12 12:30:41 +1300

    MNT Add .gitignore

--- tests/Tasks/DocblockTagWriterTaskTest/TestService.php
+++ tests/Tasks/DocblockTagWriterTaskTest/TestService.php
@@ -4,7 +4,7 @@ namespace SilverStripe\DocblockWriter\Tests\Tasks\DocblockTagWriterTaskTest;
 
 /**
  * This class does nothing, it exists purely to test that it is not processed by the task
- * because it is not a DataObject or Extension
+ * because it is not a DataObject or Extension in DocblockTagWriterTaskTest::testGetProcessableFiles()
  */
 class TestService
 {
